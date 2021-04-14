7ae011a2bf53c81484a87de8495acd7acdb32a4a
Finn Gruwier Larsen <finn@gruwier.dk>
2021-04-14 15:56:24 +0200

    Fix file extension.

--- generate_newick.py
+++ generate_newick.py
@@ -4,7 +4,7 @@ from grapetree import module
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    profile_path = dir_path + '/test_data/Achromobacter.profile'
+    profile_path = dir_path + '/test_data/Achromobacter.tsv'
     with open(profile_path, 'r') as f:
         profile = f.read()
         newick = module.MSTrees.backend(profile=profile)
