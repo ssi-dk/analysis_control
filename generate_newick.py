2aa0f03ab2200bd7049011379489e4750ab03576
Finn Gruwier Larsen <finn@gruwier.dk>
2021-04-14 11:49:15 +0200

    Use a larger test file.

--- generate_newick.py
+++ generate_newick.py
@@ -4,7 +4,7 @@ from grapetree import module
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    profile_path = dir_path + '/test_data/Grapetree_Agona.profile'
+    profile_path = dir_path + '/test_data/Achromobacter.profile'
     with open(profile_path, 'r') as f:
         profile = f.read()
         newick = module.MSTrees.backend(profile=profile)
