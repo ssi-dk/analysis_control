a8de4f99e1a3d344954e0e0697fc13be3ce59c98
Finn Gruwier Larsen <finn@gruwier.dk>
2021-09-16 13:57:09 +0200

    Import MSTrees directly.

--- commands/generate_newick.py
+++ commands/generate_newick.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from sys import stdout
 
-from grapetree import module
+from grapetree.module import MSTrees
 
 parser = argparse.ArgumentParser(description='Generate a Newick file from a file containing allele profiles.')
 parser.add_argument('profile_file', help='Full path and filename for the profile file')
@@ -10,5 +10,5 @@ args = parser.parse_args()
 
 with open(args.profile_file, 'r') as f:
     profile = f.read()
-    newick = module.MSTrees.backend(profile=profile)
+    newick = MSTrees.backend(profile=profile)
     stdout.write(newick)
