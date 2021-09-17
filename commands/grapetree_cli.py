55115a971a6ef78ef0e8cc10c00a2ecd9f1a5776
Finn Gruwier Larsen <finn@gruwier.dk>
2021-09-17 10:39:04 +0200

    Add Grapetree CLI.

---
+++ commands/grapetree_cli.py
@@ -0,0 +1,20 @@
+import sys
+
+from grapetree.module import MSTrees
+
+if len(sys.argv) == 1:
+    print("Type 'grapetree --help' to learn how to run this command. \
+        Note that with this CLI utility, all arguments must be in the long form.")
+    sys.exit()
+
+arg_list = sys.argv[1:]
+mapping = dict()
+
+while True:
+    try: 
+        mapping[arg_list.pop()[2:]] = arg_list.pop()
+    except IndexError:
+        break
+
+print(mapping)
+print(MSTrees.backend(**mapping))
