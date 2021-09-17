965974292e0f133c980ee052481f926986b0ede2
Finn Gruwier Larsen <finn@gruwier.dk>
2021-09-17 10:41:26 +0200

    Update help text.

--- commands/grapetree_cli.py
+++ commands/grapetree_cli.py
@@ -3,8 +3,8 @@ import sys
 from grapetree.module import MSTrees
 
 if len(sys.argv) == 1:
-    print("Type 'grapetree --help' to learn how to run this command. \
-        Note that with this CLI utility, all arguments must be in the long form.")
+    print("Type 'grapetree --help' to learn how to run this command. " \
+        + "Note that with this CLI utility, all arguments must be in the long form.")
     sys.exit()
 
 arg_list = sys.argv[1:]
